34f259dc824dea1d1ff4780a70160da04a51b6d4
SpacePirateWZ <spacepiratewz@gmail.com>
2023-07-30 18:16:39 +0800

    Project at 90% completion status with 2 more bugs to fix

--- script.js
+++ script.js
@@ -1,7 +1,49 @@
-const addbtn = document.querySelector('.add-btn')
+const addBtn = document.getElementById('add-btn')
+const taskContainer = document.getElementById('task-container')
+const inputText = document.getElementById('text-box')
 
-console.log(addbtn)
+addBtn.addEventListener('click',function(){
+    
+    let task = document.createElement('div');
+    task.classList.add('task');
+    
+    let li = document.createElement('li');
+    li.innerText = `${inputText.value}`;
+    task.appendChild(li);
 
-addbtn.addEventListener('click', function(e) {
-    alert('hello there!')
-})
+    let checkbtn = document.createElement('button');
+    checkbtn.innerHTML = `<i class="fa-solid fa-check"></i>`;
+    // checkbtn.innerText = "fish"; *test to see is it any text within the div will get strike-through
+    checkbtn.classList.add('checkTask');
+    task.appendChild(checkbtn);
+
+    let deletebtn = document.createElement('button');
+    deletebtn.innerHTML = `<i class="fa-solid fa-trash"></i>`;
+    deletebtn.classList.add('deleteTask');
+    task.appendChild(deletebtn);
+
+    if(inputText.value === "") {
+        alert("Please enter a task");
+    }
+    else {
+        taskContainer.appendChild(task)
+    } 
+
+    inputText.value = "";
+
+    checkbtn.addEventListener('click', function(){
+        checkbtn.parentElement.style.textDecoration = "line-through"
+    });
+
+    deletebtn.addEventListener('click', function(e){
+        let target = e.target;
+        target.parentElement.parentElement.remove();
+    });
+    
+});
+
+
+// 2 bugs to fix:
+// 1) deleting task will delete all if we didnt click on the <i>
+// 2) after deleting task, if we try to add the task, the task wont be added
+
